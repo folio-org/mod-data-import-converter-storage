bcb1a433c73dcf03bf7ad7555a36b706c2625db1
Igor_Gorchakov <Igor_Gorchakov@epam.com>
2019-03-12 16:11:43 +0200

    Added implementation for 'save' and 'getById' DAO methods

--- mod-data-import-converter-storage-server/src/main/java/org/folio/dao/association/AbstractProfileAssociationDao.java
+++ mod-data-import-converter-storage-server/src/main/java/org/folio/dao/association/AbstractProfileAssociationDao.java
@@ -1,12 +1,19 @@
 package org.folio.dao.association;
 
 import io.vertx.core.Future;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
 import org.folio.dao.PostgresClientFactory;
 import org.folio.dao.ProfileDao;
 import org.folio.rest.jaxrs.model.ProfileAssociation;
+import org.folio.rest.persist.Criteria.Criteria;
+import org.folio.rest.persist.Criteria.Criterion;
+import org.folio.rest.persist.interfaces.Results;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Optional;
+
+import static org.folio.dao.util.DaoUtil.constructCriteria;
 
 /**
  * Generic implementation of the of the {@link ProfileAssociationDao}
@@ -16,6 +23,7 @@ import java.util.Optional;
  */
 public abstract class AbstractProfileAssociationDao<M, D> implements ProfileAssociationDao<M, D> {
   protected static final String ID_FIELD = "'id'"; //NOSONAR
+  private static final Logger logger = LoggerFactory.getLogger(AbstractProfileAssociationDao.class);
   @Autowired
   protected PostgresClientFactory pgClientFactory; //NOSONAR
   protected ProfileDao masterProfileDao;  //NOSONAR
@@ -28,14 +36,24 @@ public abstract class AbstractProfileAssociationDao<M, D> implements ProfileAsso
 
   @Override
   public Future<String> save(ProfileAssociation entity, String tenantId) {
-    // STUB implementation
-    return Future.succeededFuture();
+    Future<String> future = Future.future();
+    pgClientFactory.createInstance(tenantId).save(getTableName(), entity.getId(), entity, future.completer());
+    return future;
   }
 
   @Override
   public Future<Optional<ProfileAssociation>> getById(String id, String tenantId) {
-    // STUB implementation
-    return Future.succeededFuture(Optional.of(null));
+    Future<Results<ProfileAssociation>> future = Future.future();
+    try {
+      Criteria idCrit = constructCriteria(ID_FIELD, id);
+      pgClientFactory.createInstance(tenantId).get(getTableName(), ProfileAssociation.class, new Criterion(idCrit), true, false, future.completer());
+    } catch (Exception e) {
+      logger.error("Error querying {} by id", ProfileAssociation.class.getSimpleName(), e);
+      future.fail(e);
+    }
+    return future
+      .map(Results::getResults)
+      .map(profiles -> profiles.isEmpty() ? Optional.empty() : Optional.of(profiles.get(0)));
   }
 
   @Override
