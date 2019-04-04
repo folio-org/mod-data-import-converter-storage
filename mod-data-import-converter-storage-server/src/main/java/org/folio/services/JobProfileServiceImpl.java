2e495c8eee2204e27143271fc718a9c8e6d00aa0
Ruslan Lavrov <ruslan_lavrov@epam.com>
2019-04-04 13:06:43 +0300

    Changed schema for jobProfile

--- mod-data-import-converter-storage-server/src/main/java/org/folio/services/JobProfileServiceImpl.java
+++ mod-data-import-converter-storage-server/src/main/java/org/folio/services/JobProfileServiceImpl.java
@@ -2,13 +2,10 @@ package org.folio.services;
 
 import io.vertx.core.Future;
 import org.folio.dataimport.util.OkapiConnectionParams;
-import org.folio.rest.jaxrs.model.DataType;
 import org.folio.rest.jaxrs.model.JobProfile;
 import org.folio.rest.jaxrs.model.JobProfileCollection;
 import org.springframework.stereotype.Component;
 
-import java.util.Collections;
-import java.util.List;
 import java.util.UUID;
 
 @Component
@@ -16,13 +13,11 @@ public class JobProfileServiceImpl extends AbstractProfileService<JobProfile, Jo
 
   @Override
   public Future<JobProfile> saveProfile(JobProfile profile, OkapiConnectionParams params) {
-    profile.setDataTypes(sortDataTypes(profile.getDataTypes()));
     return super.saveProfile(profile, params);
   }
 
   @Override
   public Future<JobProfile> updateProfile(JobProfile profile, OkapiConnectionParams params) {
-    profile.setDataTypes(sortDataTypes(profile.getDataTypes()));
     return super.updateProfile(profile, params);
   }
 
@@ -35,13 +30,5 @@ public class JobProfileServiceImpl extends AbstractProfileService<JobProfile, Jo
   Future<JobProfile> setUserInfoForProfile(JobProfile profile, OkapiConnectionParams params) {
     return lookupUser(profile.getMetadata().getUpdatedByUserId(), params)
       .compose(userInfo -> Future.succeededFuture(profile.withUserInfo(userInfo)));
-  }
-
-  private List<DataType> sortDataTypes(List<DataType> list) {
-    if (list == null) {
-      return Collections.emptyList();
-    }
-    Collections.sort(list);
-    return list;
   }
 }
