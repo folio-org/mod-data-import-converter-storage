72fa6bb1f4d30f8d07a7ac685502df57e93fe7c6
Ruslan Lavrov <ruslan_lavrov@epam.com>
2019-06-10 16:54:40 +0300

    Updated Action Profile sample data

--- mod-data-import-converter-storage-server/src/main/resources/templates/db_scripts/testData/test_mapping_profiles.sql
+++ mod-data-import-converter-storage-server/src/main/resources/templates/db_scripts/testData/test_mapping_profiles.sql
@@ -1,7 +1,7 @@
 INSERT INTO ${myuniversity}_${mymodule}.mapping_profiles (_id, jsonb) values
-('354f753a-02f9-4cb4-a7ea-e9154979fb7a', '{
-	"id": "354f753a-02f9-4cb4-a7ea-e9154979fb7a",
-	"name": "Test mapping profile№1",
+('0d991835-fabb-4234-8472-3ae94bc7a1b3', '{
+	"id": "0d991835-fabb-4234-8472-3ae94bc7a1b3",
+	"name": "MARC brief instance 1",
 	"description": "Lorem ipsum dolor sit amet",
 	"mapped": "Order - 1 fields",
 	"tags": {
@@ -24,11 +24,611 @@ INSERT INTO ${myuniversity}_${mymodule}.mapping_profiles (_id, jsonb) values
     "updatedByUsername": ""
    }
 }'),
-('ac82ec05-4c9d-41f9-bbe6-763992041909', '{
-	"id": "ac82ec05-4c9d-41f9-bbe6-763992041909",
-	"name": "Test mapping profile№2",
+('5b546bd4-ac38-44a8-aea1-30e460a9700f', '{
+	"id": "5b546bd4-ac38-44a8-aea1-30e460a9700f",
+	"name": "Prelim holding from Bib 1",
 	"description": "Lorem ipsum dolor sit amet",
-  "mapped": "Order - 2 fields",
+	"mapped": "Order - 1 fields",
+	"tags": {
+    "tagList": [
+		  "Lorem", "ipsum"
+	  ]
+	},
+	"deleted": false,
+  "userInfo": {
+    "firstName": "DIKU",
+    "lastName": "ADMINISTRATOR",
+    "userName": "diku_admin"
+	},
+	"metadata": {
+    "createdDate": "2018-10-30T12:41:22.000",
+    "createdByUserId": "",
+    "createdByUsername": "",
+    "updatedDate": "2018-11-02T12:09:51.000",
+    "updatedByUserId": "",
+    "updatedByUsername": ""
+   }
+}'),
+('fce00bb4-6dea-499d-8645-c55c584a2970', '{
+	"id": "fce00bb4-6dea-499d-8645-c55c584a2970",
+	"name": "Prelim item from MARC",
+	"description": "Lorem ipsum dolor sit amet",
+	"mapped": "Order - 1 fields",
+	"tags": {
+    "tagList": [
+		  "Lorem", "ipsum"
+	  ]
+	},
+	"deleted": false,
+  "userInfo": {
+    "firstName": "DIKU",
+    "lastName": "ADMINISTRATOR",
+    "userName": "diku_admin"
+	},
+	"metadata": {
+    "createdDate": "2018-10-30T12:41:22.000",
+    "createdByUserId": "",
+    "createdByUsername": "",
+    "updatedDate": "2018-11-02T12:09:51.000",
+    "updatedByUserId": "",
+    "updatedByUsername": ""
+   }
+}'),
+('0d85d496-4504-4e79-be97-c6995d101b7e', '{
+	"id": "0d85d496-4504-4e79-be97-c6995d101b7e",
+	"name": "Order from MARC 1",
+	"description": "Lorem ipsum dolor sit amet",
+	"mapped": "Order - 1 fields",
+	"tags": {
+    "tagList": [
+		  "Lorem", "ipsum"
+	  ]
+	},
+	"deleted": false,
+  "userInfo": {
+    "firstName": "DIKU",
+    "lastName": "ADMINISTRATOR",
+    "userName": "diku_admin"
+	},
+	"metadata": {
+    "createdDate": "2018-10-30T12:41:22.000",
+    "createdByUserId": "",
+    "createdByUsername": "",
+    "updatedDate": "2018-11-02T12:09:51.000",
+    "updatedByUserId": "",
+    "updatedByUsername": ""
+   }
+}'),
+('c5bccf59-cb5f-4edf-95a1-5dae28d75747', '{
+	"id": "c5bccf59-cb5f-4edf-95a1-5dae28d75747",
+	"name": "Brief MARC Bib from MARC",
+	"description": "Lorem ipsum dolor sit amet",
+	"mapped": "Order - 1 fields",
+	"tags": {
+    "tagList": [
+		  "Lorem", "ipsum"
+	  ]
+	},
+	"deleted": false,
+  "userInfo": {
+    "firstName": "DIKU",
+    "lastName": "ADMINISTRATOR",
+    "userName": "diku_admin"
+	},
+	"metadata": {
+    "createdDate": "2018-10-30T12:41:22.000",
+    "createdByUserId": "",
+    "createdByUsername": "",
+    "updatedDate": "2018-11-02T12:09:51.000",
+    "updatedByUserId": "",
+    "updatedByUsername": ""
+   }
+}'),
+('8c65ab76-5b82-471b-9b8c-43b32385e7f7', '{
+	"id": "8c65ab76-5b82-471b-9b8c-43b32385e7f7",
+	"name": "SRS MARC Bib-SR",
+	"description": "Lorem ipsum dolor sit amet",
+	"mapped": "Order - 1 fields",
+	"tags": {
+    "tagList": [
+		  "Lorem", "ipsum"
+	  ]
+	},
+	"deleted": false,
+  "userInfo": {
+    "firstName": "DIKU",
+    "lastName": "ADMINISTRATOR",
+    "userName": "diku_admin"
+	},
+	"metadata": {
+    "createdDate": "2018-10-30T12:41:22.000",
+    "createdByUserId": "",
+    "createdByUsername": "",
+    "updatedDate": "2018-11-02T12:09:51.000",
+    "updatedByUserId": "",
+    "updatedByUsername": ""
+   }
+}'),
+('7545c184-e274-4720-ad3a-cc58e1f2d2c7', '{
+	"id": "7545c184-e274-4720-ad3a-cc58e1f2d2c7",
+	"name": "SR instance",
+	"description": "Lorem ipsum dolor sit amet",
+	"mapped": "Order - 1 fields",
+	"tags": {
+    "tagList": [
+		  "Lorem", "ipsum"
+	  ]
+	},
+	"deleted": false,
+  "userInfo": {
+    "firstName": "DIKU",
+    "lastName": "ADMINISTRATOR",
+    "userName": "diku_admin"
+	},
+	"metadata": {
+    "createdDate": "2018-10-30T12:41:22.000",
+    "createdByUserId": "",
+    "createdByUsername": "",
+    "updatedDate": "2018-11-02T12:09:51.000",
+    "updatedByUserId": "",
+    "updatedByUsername": ""
+   }
+}'),
+('8fdc2cc5-3846-4572-8261-d2be5192bc5a', '{
+	"id": "8fdc2cc5-3846-4572-8261-d2be5192bc5a",
+	"name": "SR holdings-Dewey",
+	"description": "Lorem ipsum dolor sit amet",
+	"mapped": "Order - 1 fields",
+	"tags": {
+    "tagList": [
+		  "Lorem", "ipsum"
+	  ]
+	},
+	"deleted": false,
+  "userInfo": {
+    "firstName": "DIKU",
+    "lastName": "ADMINISTRATOR",
+    "userName": "diku_admin"
+	},
+	"metadata": {
+    "createdDate": "2018-10-30T12:41:22.000",
+    "createdByUserId": "",
+    "createdByUsername": "",
+    "updatedDate": "2018-11-02T12:09:51.000",
+    "updatedByUserId": "",
+    "updatedByUsername": ""
+   }
+}'),
+('ed46cfd0-b533-4ebc-88e3-6612a712a64c', '{
+	"id": "ed46cfd0-b533-4ebc-88e3-6612a712a64c",
+	"name": "SR item-normal",
+	"description": "Lorem ipsum dolor sit amet",
+	"mapped": "Order - 1 fields",
+	"tags": {
+    "tagList": [
+		  "Lorem", "ipsum"
+	  ]
+	},
+	"deleted": false,
+  "userInfo": {
+    "firstName": "DIKU",
+    "lastName": "ADMINISTRATOR",
+    "userName": "diku_admin"
+	},
+	"metadata": {
+    "createdDate": "2018-10-30T12:41:22.000",
+    "createdByUserId": "",
+    "createdByUsername": "",
+    "updatedDate": "2018-11-02T12:09:51.000",
+    "updatedByUserId": "",
+    "updatedByUsername": ""
+   }
+}'),
+('91e37f33-9d12-4cc2-93c8-7cbad031cca6', '{
+	"id": "91e37f33-9d12-4cc2-93c8-7cbad031cca6",
+	"name": "MARC invoice-1",
+	"description": "Lorem ipsum dolor sit amet",
+	"mapped": "Order - 1 fields",
+	"tags": {
+    "tagList": [
+		  "Lorem", "ipsum"
+	  ]
+	},
+	"deleted": false,
+  "userInfo": {
+    "firstName": "DIKU",
+    "lastName": "ADMINISTRATOR",
+    "userName": "diku_admin"
+	},
+	"metadata": {
+    "createdDate": "2018-10-30T12:41:22.000",
+    "createdByUserId": "",
+    "createdByUsername": "",
+    "updatedDate": "2018-11-02T12:09:51.000",
+    "updatedByUserId": "",
+    "updatedByUsername": ""
+   }
+}'),
+('0cf967db-3d75-44ae-b116-81654bd36973', '{
+	"id": "0cf967db-3d75-44ae-b116-81654bd36973",
+	"name": "SRS MARC-AP",
+	"description": "Lorem ipsum dolor sit amet",
+	"mapped": "Order - 1 fields",
+	"tags": {
+    "tagList": [
+		  "Lorem", "ipsum"
+	  ]
+	},
+	"deleted": false,
+  "userInfo": {
+    "firstName": "DIKU",
+    "lastName": "ADMINISTRATOR",
+    "userName": "diku_admin"
+	},
+	"metadata": {
+    "createdDate": "2018-10-30T12:41:22.000",
+    "createdByUserId": "",
+    "createdByUsername": "",
+    "updatedDate": "2018-11-02T12:09:51.000",
+    "updatedByUserId": "",
+    "updatedByUsername": ""
+   }
+}'),
+('6a05ac0e-e65f-43a2-8a55-cc4edd56137c', '{
+	"id": "6a05ac0e-e65f-43a2-8a55-cc4edd56137c",
+	"name": "SRS MARC Bib",
+	"description": "Lorem ipsum dolor sit amet",
+	"mapped": "Order - 1 fields",
+	"tags": {
+    "tagList": [
+		  "Lorem", "ipsum"
+	  ]
+	},
+	"deleted": false,
+  "userInfo": {
+    "firstName": "DIKU",
+    "lastName": "ADMINISTRATOR",
+    "userName": "diku_admin"
+	},
+	"metadata": {
+    "createdDate": "2018-10-30T12:41:22.000",
+    "createdByUserId": "",
+    "createdByUsername": "",
+    "updatedDate": "2018-11-02T12:09:51.000",
+    "updatedByUserId": "",
+    "updatedByUsername": ""
+   }
+}'),
+('4a887936-794d-4014-a1ec-046abfab01f6', '{
+	"id": "4a887936-794d-4014-a1ec-046abfab01f6",
+	"name": "AP Instance",
+	"description": "Lorem ipsum dolor sit amet",
+	"mapped": "Order - 1 fields",
+	"tags": {
+    "tagList": [
+		  "Lorem", "ipsum"
+	  ]
+	},
+	"deleted": false,
+  "userInfo": {
+    "firstName": "DIKU",
+    "lastName": "ADMINISTRATOR",
+    "userName": "diku_admin"
+	},
+	"metadata": {
+    "createdDate": "2018-10-30T12:41:22.000",
+    "createdByUserId": "",
+    "createdByUsername": "",
+    "updatedDate": "2018-11-02T12:09:51.000",
+    "updatedByUserId": "",
+    "updatedByUsername": ""
+   }
+}'),
+('1737d899-6fb9-44f4-ba16-9a8b111cd61d', '{
+	"id": "1737d899-6fb9-44f4-ba16-9a8b111cd61d",
+	"name": "AP holdings-MARC Bib",
+	"description": "Lorem ipsum dolor sit amet",
+	"mapped": "Order - 1 fields",
+	"tags": {
+    "tagList": [
+		  "Lorem", "ipsum"
+	  ]
+	},
+	"deleted": false,
+  "userInfo": {
+    "firstName": "DIKU",
+    "lastName": "ADMINISTRATOR",
+    "userName": "diku_admin"
+	},
+	"metadata": {
+    "createdDate": "2018-10-30T12:41:22.000",
+    "createdByUserId": "",
+    "createdByUsername": "",
+    "updatedDate": "2018-11-02T12:09:51.000",
+    "updatedByUserId": "",
+    "updatedByUsername": ""
+   }
+}'),
+('2c655027-83ee-40e9-8221-d252c9bf7b65', '{
+	"id": "2c655027-83ee-40e9-8221-d252c9bf7b65",
+	"name": "AP item from MARC",
+	"description": "Lorem ipsum dolor sit amet",
+	"mapped": "Order - 1 fields",
+	"tags": {
+    "tagList": [
+		  "Lorem", "ipsum"
+	  ]
+	},
+	"deleted": false,
+  "userInfo": {
+    "firstName": "DIKU",
+    "lastName": "ADMINISTRATOR",
+    "userName": "diku_admin"
+	},
+	"metadata": {
+    "createdDate": "2018-10-30T12:41:22.000",
+    "createdByUserId": "",
+    "createdByUsername": "",
+    "updatedDate": "2018-11-02T12:09:51.000",
+    "updatedByUserId": "",
+    "updatedByUsername": ""
+   }
+}'),
+('31cc9c07-8d38-4117-a4ec-287593ee9aa0', '{
+	"id": "31cc9c07-8d38-4117-a4ec-287593ee9aa0",
+	"name": "Create order-GOBI AP",
+	"description": "Lorem ipsum dolor sit amet",
+	"mapped": "Order - 1 fields",
+	"tags": {
+    "tagList": [
+		  "Lorem", "ipsum"
+	  ]
+	},
+	"deleted": false,
+  "userInfo": {
+    "firstName": "DIKU",
+    "lastName": "ADMINISTRATOR",
+    "userName": "diku_admin"
+	},
+	"metadata": {
+    "createdDate": "2018-10-30T12:41:22.000",
+    "createdByUserId": "",
+    "createdByUsername": "",
+    "updatedDate": "2018-11-02T12:09:51.000",
+    "updatedByUserId": "",
+    "updatedByUsername": ""
+   }
+}'),
+('156714f8-ee1b-4830-bb42-eaa39f0f54ac', '{
+	"id": "156714f8-ee1b-4830-bb42-eaa39f0f54ac",
+	"name": "Create invoice-GOBI AP MARC",
+	"description": "Lorem ipsum dolor sit amet",
+	"mapped": "Order - 1 fields",
+	"tags": {
+    "tagList": [
+		  "Lorem", "ipsum"
+	  ]
+	},
+	"deleted": false,
+  "userInfo": {
+    "firstName": "DIKU",
+    "lastName": "ADMINISTRATOR",
+    "userName": "diku_admin"
+	},
+	"metadata": {
+    "createdDate": "2018-10-30T12:41:22.000",
+    "createdByUserId": "",
+    "createdByUsername": "",
+    "updatedDate": "2018-11-02T12:09:51.000",
+    "updatedByUserId": "",
+    "updatedByUsername": ""
+   }
+}'),
+('c9a192aa-6010-4020-84db-6a67493b9a2f', '{
+	"id": "c9a192aa-6010-4020-84db-6a67493b9a2f",
+	"name": "MARC - URL only",
+	"description": "Lorem ipsum dolor sit amet",
+	"mapped": "Order - 1 fields",
+	"tags": {
+    "tagList": [
+		  "Lorem", "ipsum"
+	  ]
+	},
+	"deleted": false,
+  "userInfo": {
+    "firstName": "DIKU",
+    "lastName": "ADMINISTRATOR",
+    "userName": "diku_admin"
+	},
+	"metadata": {
+    "createdDate": "2018-10-30T12:41:22.000",
+    "createdByUserId": "",
+    "createdByUsername": "",
+    "updatedDate": "2018-11-02T12:09:51.000",
+    "updatedByUserId": "",
+    "updatedByUsername": ""
+   }
+}'),
+('1f005754-a700-4578-9108-567cb389b18b', '{
+	"id": "1f005754-a700-4578-9108-567cb389b18b",
+	"name": "Instance - URL only",
+	"description": "Lorem ipsum dolor sit amet",
+	"mapped": "Order - 1 fields",
+	"tags": {
+    "tagList": [
+		  "Lorem", "ipsum"
+	  ]
+	},
+	"deleted": false,
+  "userInfo": {
+    "firstName": "DIKU",
+    "lastName": "ADMINISTRATOR",
+    "userName": "diku_admin"
+	},
+	"metadata": {
+    "createdDate": "2018-10-30T12:41:22.000",
+    "createdByUserId": "",
+    "createdByUsername": "",
+    "updatedDate": "2018-11-02T12:09:51.000",
+    "updatedByUserId": "",
+    "updatedByUsername": ""
+   }
+}'),
+('888837b3-6081-4902-b2c7-8cc92ef621e4', '{
+	"id": "888837b3-6081-4902-b2c7-8cc92ef621e4",
+	"name": "Whole MARC-no admin data",
+	"description": "Lorem ipsum dolor sit amet",
+	"mapped": "Order - 1 fields",
+	"tags": {
+    "tagList": [
+		  "Lorem", "ipsum"
+	  ]
+	},
+	"deleted": false,
+  "userInfo": {
+    "firstName": "DIKU",
+    "lastName": "ADMINISTRATOR",
+    "userName": "diku_admin"
+	},
+	"metadata": {
+    "createdDate": "2018-10-30T12:41:22.000",
+    "createdByUserId": "",
+    "createdByUsername": "",
+    "updatedDate": "2018-11-02T12:09:51.000",
+    "updatedByUserId": "",
+    "updatedByUsername": ""
+   }
+}'),
+('c928394d-4c5d-4350-a70c-549471a0662b', '{
+	"id": "c928394d-4c5d-4350-a70c-549471a0662b",
+	"name": "Whole MARC-9xx",
+	"description": "Lorem ipsum dolor sit amet",
+	"mapped": "Order - 1 fields",
+	"tags": {
+    "tagList": [
+		  "Lorem", "ipsum"
+	  ]
+	},
+	"deleted": false,
+  "userInfo": {
+    "firstName": "DIKU",
+    "lastName": "ADMINISTRATOR",
+    "userName": "diku_admin"
+	},
+	"metadata": {
+    "createdDate": "2018-10-30T12:41:22.000",
+    "createdByUserId": "",
+    "createdByUsername": "",
+    "updatedDate": "2018-11-02T12:09:51.000",
+    "updatedByUserId": "",
+    "updatedByUsername": ""
+   }
+}'),
+('e0f069d4-ecd8-41c9-ad40-bbbdd54f39ac', '{
+	"id": "e0f069d4-ecd8-41c9-ad40-bbbdd54f39ac",
+	"name": "Update holdings-ETL",
+	"description": "Lorem ipsum dolor sit amet",
+	"mapped": "Order - 1 fields",
+	"tags": {
+    "tagList": [
+		  "Lorem", "ipsum"
+	  ]
+	},
+	"deleted": false,
+  "userInfo": {
+    "firstName": "DIKU",
+    "lastName": "ADMINISTRATOR",
+    "userName": "diku_admin"
+	},
+	"metadata": {
+    "createdDate": "2018-10-30T12:41:22.000",
+    "createdByUserId": "",
+    "createdByUsername": "",
+    "updatedDate": "2018-11-02T12:09:51.000",
+    "updatedByUserId": "",
+    "updatedByUsername": ""
+   }
+}'),
+('d46c2753-37e7-4c0e-8146-d7bca959e82c', '{
+	"id": "d46c2753-37e7-4c0e-8146-d7bca959e82c",
+	"name": "Whole MARC Authority",
+	"description": "Lorem ipsum dolor sit amet",
+	"mapped": "Order - 1 fields",
+	"tags": {
+    "tagList": [
+		  "Lorem", "ipsum"
+	  ]
+	},
+	"deleted": false,
+  "userInfo": {
+    "firstName": "DIKU",
+    "lastName": "ADMINISTRATOR",
+    "userName": "diku_admin"
+	},
+	"metadata": {
+    "createdDate": "2018-10-30T12:41:22.000",
+    "createdByUserId": "",
+    "createdByUsername": "",
+    "updatedDate": "2018-11-02T12:09:51.000",
+    "updatedByUserId": "",
+    "updatedByUsername": ""
+   }
+}'),
+('7369fd57-9d91-48b5-86ab-1b833dd4eded', '{
+	"id": "7369fd57-9d91-48b5-86ab-1b833dd4eded",
+	"name": "Create DDA MARC",
+	"description": "Lorem ipsum dolor sit amet",
+	"mapped": "Order - 1 fields",
+	"tags": {
+    "tagList": [
+		  "Lorem", "ipsum"
+	  ]
+	},
+	"deleted": false,
+  "userInfo": {
+    "firstName": "DIKU",
+    "lastName": "ADMINISTRATOR",
+    "userName": "diku_admin"
+	},
+	"metadata": {
+    "createdDate": "2018-10-30T12:41:22.000",
+    "createdByUserId": "",
+    "createdByUsername": "",
+    "updatedDate": "2018-11-02T12:09:51.000",
+    "updatedByUserId": "",
+    "updatedByUsername": ""
+   }
+}'),
+('1a8c57b1-5ec5-473b-8fc7-e8ad13194621', '{
+	"id": "1a8c57b1-5ec5-473b-8fc7-e8ad13194621",
+	"name": "Create DDA instance",
+	"description": "Lorem ipsum dolor sit amet",
+	"mapped": "Order - 1 fields",
+	"tags": {
+    "tagList": [
+		  "Lorem", "ipsum"
+	  ]
+	},
+	"deleted": false,
+  "userInfo": {
+    "firstName": "DIKU",
+    "lastName": "ADMINISTRATOR",
+    "userName": "diku_admin"
+	},
+	"metadata": {
+    "createdDate": "2018-10-30T12:41:22.000",
+    "createdByUserId": "",
+    "createdByUsername": "",
+    "updatedDate": "2018-11-02T12:09:51.000",
+    "updatedByUserId": "",
+    "updatedByUsername": ""
+   }
+}'),
+('cb6e0e3c-8748-413f-b9a6-2037522195c4', '{
+	"id": "cb6e0e3c-8748-413f-b9a6-2037522195c4",
+	"name": "Create DDA holdings",
+	"description": "Lorem ipsum dolor sit amet",
+	"mapped": "Order - 1 fields",
 	"tags": {
     "tagList": [
 		  "Lorem", "ipsum"
@@ -49,11 +649,11 @@ INSERT INTO ${myuniversity}_${mymodule}.mapping_profiles (_id, jsonb) values
     "updatedByUsername": ""
   }
 }'),
-('452434f1-0df0-4aa1-8402-6d51ec96d6a6', '{
-	"id": "452434f1-0df0-4aa1-8402-6d51ec96d6a6",
-	"name": "Test mapping profile№3",
+('2e45beb9-2aca-4f58-a42b-c121841e4331', '{
+	"id": "2e45beb9-2aca-4f58-a42b-c121841e4331",
+	"name": "Create Instance from Excel-2",
 	"description": "Lorem ipsum dolor sit amet",
-	"mapped": "Order - 3 fields",
+	"mapped": "Order - 1 fields",
 	"tags": {
     "tagList": [
 		  "Lorem", "ipsum"
@@ -74,11 +674,11 @@ INSERT INTO ${myuniversity}_${mymodule}.mapping_profiles (_id, jsonb) values
     "updatedByUsername": ""
   }
 }'),
-('79fc8de0-b094-43c5-89e0-4207420335f8', '{
-	"id": "79fc8de0-b094-43c5-89e0-4207420335f8",
-	"name": "Test mapping profile№4",
+('08f75aeb-d1b0-4a74-ab6c-9364617bae9c', '{
+	"id": "08f75aeb-d1b0-4a74-ab6c-9364617bae9c",
+	"name": "Create Holdings from Excel-2",
 	"description": "Lorem ipsum dolor sit amet",
-	"mapped": "Order - 4 fields",
+	"mapped": "Order - 1 fields",
 	"tags": {
     "tagList": [
 		  "Lorem", "ipsum"
@@ -99,11 +699,11 @@ INSERT INTO ${myuniversity}_${mymodule}.mapping_profiles (_id, jsonb) values
     "updatedByUsername": ""
   }
 }'),
-('f4f075c6-67c9-411e-af24-01ceecd81b4b', '{
-	"id": "f4f075c6-67c9-411e-af24-01ceecd81b4b",
-	"name": "Test mapping profile№5",
+('e7fc165e-a16a-46d4-a608-fd15d5b17a8d', '{
+	"id": "e7fc165e-a16a-46d4-a608-fd15d5b17a8d",
+	"name": "Create Item from Excel-2",
 	"description": "Lorem ipsum dolor sit amet",
-	"mapped": "Order - 5 fields",
+	"mapped": "Order - 1 fields",
 	"tags": {
     "tagList": [
 		  "Lorem", "ipsum"
@@ -124,11 +724,36 @@ INSERT INTO ${myuniversity}_${mymodule}.mapping_profiles (_id, jsonb) values
     "updatedByUsername": ""
   }
 }'),
-('99b7bb53-a629-4058-a5f4-d2fcf5dc7125', '{
-	"id": "99b7bb53-a629-4058-a5f4-d2fcf5dc7125",
-	"name": "Test mapping profile№6",
+('7b475a5e-4ac3-4ebe-bf4f-5d0ab32b94a5', '{
+	"id": "7b475a5e-4ac3-4ebe-bf4f-5d0ab32b94a5",
+	"name": "Create Order from Excel-2",
 	"description": "Lorem ipsum dolor sit amet",
-	"mapped": "Order - 6 fields",
+	"mapped": "Order - 1 fields",
+	"tags": {
+    "tagList": [
+		  "Lorem", "ipsum"
+	  ]
+	},
+	"deleted": false,
+  "userInfo": {
+    "firstName": "DIKU",
+    "lastName": "ADMINISTRATOR",
+    "userName": "diku_admin"
+	},
+	"metadata": {
+    "createdDate": "2018-10-30T12:41:22.000",
+    "createdByUserId": "",
+    "createdByUsername": "",
+    "updatedDate": "2018-11-02T12:09:51.000",
+    "updatedByUserId": "",
+    "updatedByUsername": ""
+  }
+}'),
+('ecdba48b-11f8-441a-b852-f075810db54b', '{
+	"id": "ecdba48b-11f8-441a-b852-f075810db54b",
+	"name": "EDI invoice",
+	"description": "Lorem ipsum dolor sit amet",
+	"mapped": "Order - 1 fields",
 	"tags": {
     "tagList": [
 		  "Lorem", "ipsum"
