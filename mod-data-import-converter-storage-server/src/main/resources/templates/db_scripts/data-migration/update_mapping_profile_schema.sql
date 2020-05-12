5c99487f70dcbc68c4cdd0b66b1fb2fb82384847
Ruslan Lavrov <ruslan_lavrov@epam.com>
2020-05-12 10:55:35 +0300

    Moved scripts to one file

--- mod-data-import-converter-storage-server/src/main/resources/templates/db_scripts/data-migration/update_mapping_profile_schema.sql
+++
@@ -1,3 +0,0 @@
--- rename field "folioRecord" to "existingRecordType"
-UPDATE ${myuniversity}_${mymodule}.mapping_profiles
-SET jsonb = jsonb - 'folioRecord' || jsonb_build_object('existingRecordType', jsonb ->> 'folioRecord');
