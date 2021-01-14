77e1ebe730fe53938a1910e588c31c13935028ea
VRohach <Volodymyr_Rohach@epam.com>
2021-01-14 16:41:12 +0200

    MODDICONV-163: Add default job profile for creating instances and SRS MARC Bibs. 1. Default job profile created. 2. ModTenantAPI updated. 2. NEWS updated.

--- NEWS.md
+++ NEWS.md
@@ -3,6 +3,7 @@
 * [MODDICONV-149](https://issues.folio.org/browse/MODDICONV-149) Create default OCLC Update Instance profile
 * [MODDICONV-158](https://issues.folio.org/browse/MODDICONV-158) Update the names and descriptions of the default OCLC profiles.
 * [MODDICONV-160](https://issues.folio.org/browse/MODDICONV-160) Update the names and descriptions of the default OCLC match, action, field mapping profiles.
+* [MODDICONV-163](https://issues.folio.org/browse/MODDICONV-163) Add default job profile for creating instances and SRS MARC Bibs.
 
 ## 2020-11-06 v1.9.2
 * [MODDICONV-152](https://issues.folio.org/browse/MODDICONV-150) Upgrade to RMB v31.1.5
--- mod-data-import-converter-storage-server/src/main/java/org/folio/rest/impl/ModTenantAPI.java
+++ mod-data-import-converter-storage-server/src/main/java/org/folio/rest/impl/ModTenantAPI.java
@@ -28,6 +28,7 @@ public class ModTenantAPI extends TenantAPI {
   private static final String DEFAULT_OCLC_JOB_PROFILE_SQL = "templates/db_scripts/defaultData/default_oclc_job_profile.sql";
   private static final String DEFAULT_OCLC_UPDATE_JOB_PROFILE_SQL = "templates/db_scripts/defaultData/default_oclc_update_job_profile.sql";
   private static final String DEFAULT_MARC_FIELD_PROTECTION_SETTINGS_SQL = "templates/db_scripts/defaultData/default_marc_field_protection_settings.sql";
+  private static final String DEFAULT_INSTANCE_AND_MARC_BIB_CREATE_JOB_PROFILE = "templates/db_scripts/defaultData/default_instance_and_srs_marc_bib_create_job_profile.sql";
   private static final String TENANT_PLACEHOLDER = "${myuniversity}";
   private static final String MODULE_PLACEHOLDER = "${mymodule}";
 
@@ -42,6 +43,7 @@ public class ModTenantAPI extends TenantAPI {
           .compose(r -> setupDefaultData(DEFAULT_MARC_FIELD_PROTECTION_SETTINGS_SQL, headers, context))
           .compose(d -> setupDefaultData(DEFAULT_OCLC_JOB_PROFILE_SQL, headers, context))
           .compose(u -> setupDefaultData(DEFAULT_OCLC_UPDATE_JOB_PROFILE_SQL, headers, context))
+          .compose(m -> setupDefaultData(DEFAULT_INSTANCE_AND_MARC_BIB_CREATE_JOB_PROFILE, headers, context))
           .onComplete(event -> handlers.handle(ar));
       }
     }, context);
