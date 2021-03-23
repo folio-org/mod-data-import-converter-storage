287a9843d3c0be6b4aa3f38450ddc3fecf437aa1
Natalia_Zaitseva <Natalia_Zaitseva1@epam.com>
2021-03-23 21:55:00 +0200

    MODDICONV-174 - Create default profile for importing new MARC Authority records
    * created default job profile for MARC Authority records

--- mod-data-import-converter-storage-server/src/main/java/org/folio/rest/impl/ModTenantAPI.java
+++ mod-data-import-converter-storage-server/src/main/java/org/folio/rest/impl/ModTenantAPI.java
@@ -30,6 +30,7 @@ public class ModTenantAPI extends TenantAPI {
   private static final String DEFAULT_MARC_FIELD_PROTECTION_SETTINGS_SQL = "templates/db_scripts/defaultData/default_marc_field_protection_settings.sql";
   private static final String DEFAULT_INSTANCE_AND_MARC_BIB_CREATE_JOB_PROFILE = "templates/db_scripts/defaultData/default_instance_and_srs_marc_bib_create_job_profile.sql";
   private static final String DEFAULT_EDIFACT_MAPPING_PROFILES = "templates/db_scripts/defaultData/default_edifact_mapping_profiles.sql";
+  private static final String DEFAULT_MARC_AUTHORITY_CREATE_JOB_PROFILE = "templates/db_scripts/defaultData/default_marc_authority_job_profile.sql";
   private static final String TENANT_PLACEHOLDER = "${myuniversity}";
   private static final String MODULE_PLACEHOLDER = "${mymodule}";
 
@@ -42,6 +43,7 @@ public class ModTenantAPI extends TenantAPI {
         .compose(u -> setupDefaultData(DEFAULT_OCLC_UPDATE_JOB_PROFILE_SQL, headers, context))
         .compose(m -> setupDefaultData(DEFAULT_INSTANCE_AND_MARC_BIB_CREATE_JOB_PROFILE, headers, context))
         .compose(m -> setupDefaultData(DEFAULT_EDIFACT_MAPPING_PROFILES, headers, context))
+        .compose(m -> setupDefaultData(DEFAULT_MARC_AUTHORITY_CREATE_JOB_PROFILE, headers, context))
         .map(num));
   }
 
