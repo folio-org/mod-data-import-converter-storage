2a0d8b34619f3a2dd221bdd7384c6544ab565089
Oleksandr Dekin <Oleksandr_Dekin@epam.com>
2021-07-21 12:36:15 +0300

    MODDICONV-193 Create default profile for importing new MARC Holdings
    - created job profile for marc holdings
    - covered data by test case

--- mod-data-import-converter-storage-server/src/main/java/org/folio/rest/impl/ModTenantAPI.java
+++ mod-data-import-converter-storage-server/src/main/java/org/folio/rest/impl/ModTenantAPI.java
@@ -29,6 +29,7 @@ public class ModTenantAPI extends TenantAPI {
   private static final String DEFAULT_INSTANCE_AND_MARC_BIB_CREATE_JOB_PROFILE = "templates/db_scripts/defaultData/default_instance_and_marc_bib_create_job_profile.sql";
   private static final String DEFAULT_EDIFACT_MAPPING_PROFILES = "templates/db_scripts/defaultData/default_edifact_mapping_profiles.sql";
   private static final String DEFAULT_MARC_AUTHORITY_CREATE_JOB_PROFILE = "templates/db_scripts/defaultData/default_marc_authority_job_profile.sql";
+  private static final String DEFAULT_MARC_HOLDINGS_CREATE_JOB_PROFILE = "templates/db_scripts/defaultData/default_marc_holdings_job_profile.sql";
   private static final String TENANT_PLACEHOLDER = "${myuniversity}";
   private static final String MODULE_PLACEHOLDER = "${mymodule}";
 
@@ -43,6 +44,7 @@ public class ModTenantAPI extends TenantAPI {
         .compose(m -> setupDefaultData(DEFAULT_INSTANCE_AND_MARC_BIB_CREATE_JOB_PROFILE, headers, context))
         .compose(m -> setupDefaultData(DEFAULT_EDIFACT_MAPPING_PROFILES, headers, context))
         .compose(m -> setupDefaultData(DEFAULT_MARC_AUTHORITY_CREATE_JOB_PROFILE, headers, context))
+        .compose(m -> setupDefaultData(DEFAULT_MARC_HOLDINGS_CREATE_JOB_PROFILE, headers, context))
         .compose(m -> setupDefaultData(UPDATE_DEFAULT_QM_INSTANCE_AND_SRS_MARC_BIB_CREATE_JOB_PROFILE, headers, context))
         .map(num));
   }
--- mod-data-import-converter-storage-server/src/test/java/org/folio/rest/impl/DefaultJobProfileTest.java
+++ mod-data-import-converter-storage-server/src/test/java/org/folio/rest/impl/DefaultJobProfileTest.java
@@ -16,6 +16,7 @@ public class DefaultJobProfileTest extends AbstractRestVerticleTest {
 
 
   private static final String DEFAULT_MARC_AUTHORITY_PROFILE_ID = "6eefa4c6-bbf7-4845-ad82-de7fc5abd0e3";
+  private static final String DEFAULT_MARC_HOLDINGS_PROFILE_ID = "80898dee-449f-44dd-9c8e-37d5eb469b1d";
 
   @Test
   public void shouldReturnDefaultProfilesListOnGet() {
@@ -25,7 +26,7 @@ public class DefaultJobProfileTest extends AbstractRestVerticleTest {
       .get(JOB_PROFILES_PATH)
       .then()
       .statusCode(HttpStatus.SC_OK)
-      .body("totalRecords", is(5));
+      .body("totalRecords", is(6));
   }
 
   @Test
@@ -37,6 +38,18 @@ public class DefaultJobProfileTest extends AbstractRestVerticleTest {
       .then()
       .statusCode(HttpStatus.SC_OK).extract().as(JobProfile.class);
     Assert.assertEquals( "Default - Create SRS MARC Authority", profile.getName());
+    Assert.assertEquals( JobProfile.DataType.MARC, profile.getDataType());
+  }
+  @Test
+
+  public void shouldReturnMarcHoldingsProfileOnGetById() {
+    final var profile = RestAssured.given()
+      .spec(spec)
+      .when()
+      .get(JOB_PROFILES_PATH + "/" + DEFAULT_MARC_HOLDINGS_PROFILE_ID)
+      .then()
+      .statusCode(HttpStatus.SC_OK).extract().as(JobProfile.class);
+    Assert.assertEquals( "Default - Create SRS MARC Holdings", profile.getName());
     Assert.assertEquals( JobProfile.DataType.MARC, profile.getDataType());
   }
 }
