66eb10910b9be2776d5adc4ffa53cc9df985bc4e
psmahin <Pavlo_Smahin@epam.com>
2022-03-16 12:28:11 +0200

    MODDICONV-238 Delete mapping from dafault authority delete job profile.

--- mod-data-import-converter-storage-server/src/main/resources/templates/db_scripts/defaultData/default_delete_marc_authority_job_profile.sql
+++ mod-data-import-converter-storage-server/src/main/resources/templates/db_scripts/defaultData/default_delete_marc_authority_job_profile.sql
@@ -21,37 +21,6 @@ INSERT INTO ${myuniversity}_${mymodule}.job_profiles (id, jsonb) values
   }
 }') ON CONFLICT DO NOTHING;
 
-INSERT INTO ${myuniversity}_${mymodule}.mapping_profiles (id, jsonb) values
-  ('ff029a0a-82ff-486d-b2b1-7a4ef4cb7988', '{
-  "id": "ff029a0a-82ff-486d-b2b1-7a4ef4cb7988",
-  "name": "Default - Delete MARC Authority record via MARC Authority app",
-  "description": "Description: This field mapping profile is used with FOLIO''s default action profile for deleting an MARC authority record via the MARC authority app. This profile deletes the authority record stored in source-record-storage and mod-inventory-storage. This field mapping profile cannot be edited, duplicated, or deleted.",
-  "incomingRecordType": "MARC_AUTHORITY",
-  "existingRecordType": "MARC_AUTHORITY",
-  "deleted": false,
-  "hidden": true,
-  "userInfo": {
-    "firstName": "System",
-    "lastName": "System",
-    "userName": "System"
-  },
-  "marcFieldProtectionSettings": [],
-  "parentProfiles": [],
-  "childProfiles": [],
-  "mappingDetails": {
-    "name": "marcAuthority",
-    "recordType": "MARC_AUTHORITY",
-    "marcMappingDetails": []
-  },
-  "metadata": {
-    "createdDate": "2022-02-16T14:00:00.000",
-    "updatedDate": "2022-02-16T15:00:00.462+0000",
-    "createdByUserId": "00000000-0000-0000-0000-000000000000",
-    "updatedByUserId": "00000000-0000-0000-0000-000000000000"
-  }
-}')
-ON CONFLICT DO NOTHING;
-
 INSERT INTO ${myuniversity}_${mymodule}.action_profiles (id, jsonb) values
   ('fabd9a3e-33c3-49b7-864d-c5af830d9990', '{
   "id": "fabd9a3e-33c3-49b7-864d-c5af830d9990",
@@ -179,15 +148,3 @@ INSERT INTO ${myuniversity}_${mymodule}.match_to_action_profiles (id, jsonb) val
     "jobProfileId": "1a338fcd-3efc-4a03-b007-394eeb0d5fb9"
 }
 ') ON CONFLICT DO NOTHING;
-
-INSERT INTO ${myuniversity}_${mymodule}.action_to_mapping_profiles (id, jsonb) values
-  ('8c28c374-1646-4487-b5ea-d0fa326dd9e7', '{
-    "id": "8c28c374-1646-4487-b5ea-d0fa326dd9e7",
-    "masterProfileId": "fabd9a3e-33c3-49b7-864d-c5af830d9990",
-    "detailProfileId": "ff029a0a-82ff-486d-b2b1-7a4ef4cb7988",
-    "order": 0,
-    "triggered": false,
-    "masterProfileType": "ACTION_PROFILE",
-    "detailProfileType": "MAPPING_PROFILE"
-}')
-ON CONFLICT DO NOTHING;
