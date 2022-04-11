f7ff7a2393be0e5047988fed8835a83cd8afb153
Satishkumar_Purswani <Satishkumar_Purswani@epam.com>
2022-04-11 13:12:46 +0530

    MODDICONV-210 - Associations_view changes to incorporate indirection relationship between Job and Action Profiles.

--- mod-data-import-converter-storage-server/src/main/resources/templates/db_scripts/associations_view.sql
+++ mod-data-import-converter-storage-server/src/main/resources/templates/db_scripts/associations_view.sql
@@ -22,8 +22,16 @@ CREATE OR REPLACE VIEW associations_view
     FROM get_profile_association_snapshot('action_to_action_profiles', 'action_profiles', 'ACTION_PROFILE', 'action_profiles', 'ACTION_PROFILE')
       UNION ALL
     SELECT association_id, master_id, master, master_type, detail_id, detail_type, detail_order, detail,  react_to, job_profile_id
-    FROM get_profile_association_snapshot('action_to_mapping_profiles', 'action_profiles', 'ACTION_PROFILE', 'mapping_profiles', 'MAPPING_PROFILE');
-
+    FROM get_profile_association_snapshot('action_to_mapping_profiles', 'action_profiles', 'ACTION_PROFILE', 'mapping_profiles', 'MAPPING_PROFILE')
+      UNION ALL
+    SELECT ASSOCIATION.ID AS association_id, CAST(ASSOCIATION.JSONB ->> 'jobProfileId' AS UUID) AS master_id, json_agg(JOB.jsonb) AS master, 'JOB_PROFILE' AS master_type, DETAIL.ID AS detail_id, 'ACTION_PROFILE' AS detail_type, 0 AS detail_order, CAST( MASTER.JSONB AS JSON ) AS detail, null AS react_to, CAST(ASSOCIATION.JSONB ->> 'jobProfileId' AS UUID) AS job_profile_id
+    FROM MATCH_TO_ACTION_PROFILES ASSOCIATION
+    INNER JOIN MATCH_PROFILES MASTER ON MASTER.ID = ASSOCIATION.MASTERPROFILEID
+    INNER JOIN ACTION_PROFILES DETAIL ON DETAIL.ID = ASSOCIATION.DETAILPROFILEID
+    INNER JOIN JOB_PROFILES JOB ON JOB.ID = CAST(ASSOCIATION.JSONB ->> 'jobProfileId' AS UUID)
+    WHERE ASSOCIATION.JSONB ->> 'detailProfileType' = 'ACTION_PROFILE'
+    	AND ASSOCIATION.JSONB ->> 'masterProfileType' = 'MATCH_PROFILE'
+    GROUP BY ASSOCIATION.ID, MASTER.ID, DETAIL.ID, DETAIL.JSONB;
 
 -- Script to create rule which will triggered upon delete query to associations_view view.
 CREATE OR REPLACE RULE delete_associations_with_details AS
