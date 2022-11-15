de677965c8907a58968dadef25ae5504a8cd632b
Kateryna Senchenko <42532596+KaterynaSenchenko@users.noreply.github.com>
2022-11-15 11:26:04 +0200

    Update mod-data-import-converter-storage-server/src/main/resources/templates/db_scripts/data-migration/set_administrative_notes.sql
    Co-authored-by: Ruslan Lavrov <47384893+RuslanLavrov@users.noreply.github.com>

--- mod-data-import-converter-storage-server/src/main/resources/templates/db_scripts/data-migration/set_administrative_notes.sql
+++ mod-data-import-converter-storage-server/src/main/resources/templates/db_scripts/data-migration/set_administrative_notes.sql
@@ -2,7 +2,7 @@
 -- change values: set administrative notes for mapping profiles, which haven't one
 UPDATE ${myuniversity}_${mymodule}.mapping_profiles
 SET jsonb = jsonb_insert(jsonb, '{mappingDetails,mappingFields,0}', '{"name": "administrativeNotes", "path": "instance.administrativeNotes[]", "value": "", "enabled": "true", "subfields": []}')
-WHERE (jsonb ->> 'mappingDetails')::json ->> 'recordType' = 'INSTANCE' AND (jsonb ->> 'mappingDetails')::json ->> 'mappingFields' NOT LIKE '%"name": "administrativeNotes"%';
+WHERE jsonb -> 'mappingDetails' ->> 'recordType' = 'INSTANCE' AND jsonb -> 'mappingDetails' ->> 'mappingFields' NOT LIKE '%"name": "administrativeNotes"%';
 
 UPDATE ${myuniversity}_${mymodule}.mapping_profiles
 SET jsonb = jsonb_insert(jsonb, '{mappingDetails,mappingFields,0}', '{"name": "administrativeNotes", "path": "item.administrativeNotes[]", "value": "", "enabled": "true", "subfields": []}')
