bdab2519e13f16012e5752f791a6054a35c1e067
RomanChernetskyi <111740564+romanchernetskyi@users.noreply.github.com>
2022-11-15 11:55:31 +0200

    [MODDICONV-276] Added migration script for administartive notes (#221)
    * Added migration script for administartive notes
    
    * update script
    
    * Update mod-data-import-converter-storage-server/src/main/resources/templates/db_scripts/data-migration/set_administrative_notes.sql
    
    Co-authored-by: Ruslan Lavrov <47384893+RuslanLavrov@users.noreply.github.com>
    
    * Refactored explicit cast
    
    Co-authored-by: Kateryna_Senchenko <kateryna_senchenko@epam.com>
    Co-authored-by: Kateryna Senchenko <42532596+KaterynaSenchenko@users.noreply.github.com>
    Co-authored-by: Ruslan Lavrov <47384893+RuslanLavrov@users.noreply.github.com>
    
    (cherry picked from commit 08ab119d58123dbb67d891293629c6389ccd4c1d)

---
+++ mod-data-import-converter-storage-server/src/main/resources/templates/db_scripts/data-migration/set_administrative_notes.sql
@@ -0,0 +1,13 @@
+-- mapping_profiles:
+-- change values: set administrative notes for mapping profiles, which haven't one
+UPDATE ${myuniversity}_${mymodule}.mapping_profiles
+SET jsonb = jsonb_insert(jsonb, '{mappingDetails,mappingFields,0}', '{"name": "administrativeNotes", "path": "instance.administrativeNotes[]", "value": "", "enabled": "true", "subfields": []}')
+WHERE jsonb -> 'mappingDetails' ->> 'recordType' = 'INSTANCE' AND jsonb -> 'mappingDetails' ->> 'mappingFields' NOT LIKE '%"name": "administrativeNotes"%';
+
+UPDATE ${myuniversity}_${mymodule}.mapping_profiles
+SET jsonb = jsonb_insert(jsonb, '{mappingDetails,mappingFields,0}', '{"name": "administrativeNotes", "path": "item.administrativeNotes[]", "value": "", "enabled": "true", "subfields": []}')
+WHERE jsonb -> 'mappingDetails' ->> 'recordType' = 'ITEM' AND jsonb -> 'mappingDetails' ->> 'mappingFields' NOT LIKE '%"name": "administrativeNotes"%';
+
+UPDATE ${myuniversity}_${mymodule}.mapping_profiles
+SET jsonb = jsonb_insert(jsonb, '{mappingDetails,mappingFields,0}', '{"name": "administrativeNotes", "path": "holdings.administrativeNotes[]", "value": "", "enabled": "true", "subfields": []}')
+WHERE jsonb -> 'mappingDetails' ->> 'recordType' = 'HOLDINGS' AND jsonb -> 'mappingDetails' ->> 'mappingFields' NOT LIKE '%"name": "administrativeNotes"%';
