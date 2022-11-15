87b3a1e33e7430beef5153c05d0f039fb3ba167f
Kateryna_Senchenko <kateryna_senchenko@epam.com>
2022-11-15 12:31:56 +0200

    update script

--- mod-data-import-converter-storage-server/src/main/resources/templates/db_scripts/data-migration/set_administrative_notes.sql
+++ mod-data-import-converter-storage-server/src/main/resources/templates/db_scripts/data-migration/set_administrative_notes.sql
@@ -1,13 +1,13 @@
 -- mapping_profiles:
 -- change values: set administrative notes for mapping profiles, which haven't one
 UPDATE ${myuniversity}_${mymodule}.mapping_profiles
-SET jsonb = jsonb_insert(jsonb, '{mappingDetails,mappingFields,0}', '{"name": "administrativeNotes", "path": "instance.administrativeNotes[]", "value": "", "enabled": "true", "subfields": []}')
+SET jsonb = jsonb_insert(jsonb, '{mappingDetails,mappingFields,9}', '{"name": "administrativeNotes", "path": "instance.administrativeNotes[]", "value": "", "enabled": "true", "subfields": []}')
 WHERE jsonb -> 'mappingDetails' ->> 'recordType' = 'INSTANCE' AND jsonb -> 'mappingDetails' ->> 'mappingFields' NOT LIKE '%"name": "administrativeNotes"%';
 
 UPDATE ${myuniversity}_${mymodule}.mapping_profiles
-SET jsonb = jsonb_insert(jsonb, '{mappingDetails,mappingFields,0}', '{"name": "administrativeNotes", "path": "item.administrativeNotes[]", "value": "", "enabled": "true", "subfields": []}')
+SET jsonb = jsonb_insert(jsonb, '{mappingDetails,mappingFields,7}', '{"name": "administrativeNotes", "path": "item.administrativeNotes[]", "value": "", "enabled": "true", "subfields": []}')
 WHERE jsonb -> 'mappingDetails' ->> 'recordType' = 'ITEM' AND jsonb -> 'mappingDetails' ->> 'mappingFields' NOT LIKE '%"name": "administrativeNotes"%';
 
 UPDATE ${myuniversity}_${mymodule}.mapping_profiles
-SET jsonb = jsonb_insert(jsonb, '{mappingDetails,mappingFields,0}', '{"name": "administrativeNotes", "path": "holdings.administrativeNotes[]", "value": "", "enabled": "true", "subfields": []}')
+SET jsonb = jsonb_insert(jsonb, '{mappingDetails,mappingFields,5}', '{"name": "administrativeNotes", "path": "holdings.administrativeNotes[]", "value": "", "enabled": "true", "subfields": []}')
 WHERE jsonb -> 'mappingDetails' ->> 'recordType' = 'HOLDINGS' AND jsonb -> 'mappingDetails' ->> 'mappingFields' NOT LIKE '%"name": "administrativeNotes"%';
